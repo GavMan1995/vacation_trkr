118bf441ed23c72acefbe4cef55d7cf644e6d091
GavMan1995 <gavyncaldwell@gmail.com>
2016-03-03 19:47:31 -0700

    got the add info to work

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,6 +1,5 @@
 class InfosController < ApplicationController
   before_action :find_trip
-
   def index
     @infos = @trip.infos
   end
@@ -10,12 +9,18 @@ class InfosController < ApplicationController
   end
 
   def new
+    @info = @trip.infos.new
+  end
+
+  def create
+    @info = @trip.infos.create(info_params)
+    redirect_to trip_infos_path(@trip)
   end
 
   private
 
   def info_params
-    params.require(:info).permit
+    params.require(:info).permit(:places, :comments)
   end
 
   def find_trip
